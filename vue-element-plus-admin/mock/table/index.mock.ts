445dbacb9f542ac3f7a3915e8847797c3814a1b9
amber-yl <903582377@qq.com>
2025-05-28 16:41:56 +0800

    feat: 提交mock接口数据

--- vue-element-plus-admin/mock/table/index.mock.ts
+++ vue-element-plus-admin/mock/table/index.mock.ts
@@ -5,32 +5,49 @@ import { toAnyString } from '@/utils'
 const timeout = 1000
 const count = 100
 
-const baseContent =
-  '<p>I am testing data, I am testing data.</p><p><img src="https://wpimg.wallstcn.com/4c69009c-0fd4-4153-b112-6cb53d1cf943"></p>'
-
-interface ListProps {
-  id: string
-  author: string
-  title: string
-  content: string
-  importance: number
-  display_time: any
-  pageviews: number
-  image_uri: string
-  video_uri?: string
+enum Type {
+  npu = 'npu',
+  gpu = 'gpu'
 }
 
-interface TreeListProps {
-  id: string
-  author: string
-  title: string
-  content: string
-  importance: number
-  display_time: any
-  image_uri: string
-  pageviews: number
-  video_uri?: string
-  children?: TreeListProps[]
+interface ListProps {
+  id?: string
+  timestamp?: number
+  created_at: string
+  updated_at: string
+  name: string // 硬件名称
+  type: Type // 硬件类型
+  matrix: {
+    float16: {
+      tflops: number
+      calibration_coefficient: number
+    }
+  }
+  vector: {
+    float16: {
+      tflops: number
+      calibration_coefficient: number
+    }
+  }
+  men1: {
+    GiB: number
+    GiBps: number
+    cube_calibration_coefficient: number
+    vector_calibration_coefficient: number
+  }
+  men2: {
+    GiB: number
+    GiBps: number
+    cube_calibration_coefficient?: number
+    vector_calibration_coefficient?: number
+  }
+  processing_mode: string
+  netWorks: {
+    bandWidth: number
+    efficiency: number
+    size: number
+    latency: number
+  }[]
 }
 
 let List: ListProps[] = []
@@ -39,184 +56,61 @@ for (let i = 0; i < count; i++) {
   List.push(
     Mock.mock({
       id: toAnyString(),
-      // timestamp: +Mock.Random.date('T'),
-      author: '@first',
-      title: '@title(5, 10)',
-      content: baseContent,
-      importance: '@integer(1, 3)',
-      display_time: '@datetime',
-      pageviews: '@integer(100, 500)',
-      image_uri: Mock.Random.image('@integer(100, 500)x@integer(100, 500)'),
-      video_uri:
-        '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-720p.mp4'
-    })
-  )
-}
-
-const treeList: TreeListProps[] = []
-
-for (let i = 0; i < count; i++) {
-  treeList.push(
-    Mock.mock({
-      id: toAnyString(),
-      // timestamp: +Mock.Random.date('T'),
-      author: '@first',
-      title: '@title(5, 10)',
-      content: baseContent,
-      importance: '@integer(1, 3)',
-      display_time: '@datetime',
-      pageviews: '@integer(300, 5000)',
-      image_uri: Mock.Random.image('@integer(100, 500)x@integer(100, 500)'),
-      children: [
+      timestamp: +Mock.Random.date('T'),
+      created_at: '@datetime',
+      updated_at: '@datetime',
+      name: '@cname',
+      type: Mock.Random.pick([Type.npu, Type.gpu]),
+      matrix: {
+        float16: {
+          tflops: '@integer(100, 1000)',
+          calibration_coefficient: '@float(0, 1, 2, 2)'
+        }
+      },
+      vector: {
+        float16: {
+          tflops: '@integer(1, 100)',
+          calibration_coefficient: '@float(0, 1, 2, 2)'
+        }
+      },
+      men1: {
+        GiB: '@integer(1, 100)',
+        GiBps: '@integer(100, 1000)',
+        cube_calibration_coefficient: '@float(0, 1, 2, 2)',
+        vector_calibration_coefficient: '@float(0, 1, 2, 2)'
+      },
+      men2: {
+        GiB: '@float(100, 1000)',
+        GiBps: '@float(1, 100, 2, 2)'
+      },
+      processing_mode: '@pick(["fp32", "fp16", "roofline"])',
+      netWorks: [
         {
-          id: toAnyString(),
-          // timestamp: +Mock.Random.date('T'),
-          author: '@first',
-          title: '@title(5, 10)',
-          content: baseContent,
-          importance: '@integer(1, 3)',
-          display_time: '@datetime',
-          pageviews: '@integer(300, 5000)',
-          image_uri: Mock.Random.image('@integer(100, 500)x@integer(100, 500)'),
-          children: [
-            {
-              id: toAnyString(),
-              // timestamp: +Mock.Random.date('T'),
-              author: '@first',
-              title: '@title(5, 10)',
-              content: baseContent,
-              importance: '@integer(1, 3)',
-              display_time: '@datetime',
-              pageviews: '@integer(300, 5000)',
-              image_uri: Mock.Random.image('@integer(100, 500)x@integer(100, 500)')
-            },
-            {
-              id: toAnyString(),
-              // timestamp: +Mock.Random.date('T'),
-              author: '@first',
-              title: '@title(5, 10)',
-              content: baseContent,
-              importance: '@integer(1, 3)',
-              display_time: '@datetime',
-              pageviews: '@integer(300, 5000)',
-              image_uri: Mock.Random.image('@integer(100, 500)x@integer(100, 500)')
-            }
-          ]
-        },
-        {
-          id: toAnyString(),
-          // timestamp: +Mock.Random.date('T'),
-          author: '@first',
-          title: '@title(5, 10)',
-          content: baseContent,
-          importance: '@integer(1, 3)',
-          display_time: '@datetime',
-          pageviews: '@integer(300, 5000)',
-          image_uri: Mock.Random.image('@integer(100, 500)x@integer(100, 500)')
-        },
-        {
-          id: toAnyString(),
-          // timestamp: +Mock.Random.date('T'),
-          author: '@first',
-          title: '@title(5, 10)',
-          content: baseContent,
-          importance: '@integer(1, 3)',
-          display_time: '@datetime',
-          pageviews: '@integer(300, 5000)',
-          image_uri: Mock.Random.image('@integer(100, 500)x@integer(100, 500)')
-        },
-        {
-          id: toAnyString(),
-          // timestamp: +Mock.Random.date('T'),
-          author: '@first',
-          title: '@title(5, 10)',
-          content: baseContent,
-          importance: '@integer(1, 3)',
-          display_time: '@datetime',
-          pageviews: '@integer(300, 5000)',
-          image_uri: Mock.Random.image('@integer(100, 500)x@integer(100, 500)')
+          bandWidth: '@integer(100, 1000)',
+          efficiency: '@float(0, 1, 2, 2)',
+          size: '@integer(100, 1000)',
+          latency: '@float(0, 1, 2, 2)'
         }
       ]
-      // image_uri
     })
   )
 }
 
-const cardList = [
-  {
-    logo: 'https://gw.alipayobjects.com/zos/rmsportal/WdGqmHpayyMjiEhcKoVE.png',
-    name: 'Alipay',
-    desc: '在中台产品的研发过程中，会出现不同的设计规范和实现方式，但其中往往存在很多类似的页面和组件，这些类似的组件会被抽离成一套标准规范。'
-  },
-  {
-    logo: 'https://gw.alipayobjects.com/zos/rmsportal/zOsKZmFRdUtvpqCImOVY.png',
-    name: 'Angular',
-    desc: '在中台产品的研发过程中，会出现不同的设计规范和实现方式，但其中往往存在很多类似的页面和组件，这些类似的组件会被抽离成一套标准规范。'
-  },
-  {
-    logo: 'https://gw.alipayobjects.com/zos/rmsportal/siCrBXXhmvTQGWPNLBow.png',
-    name: 'Bootstrap',
-    desc: '在中台产品的研发过程中，会出现不同的设计规范和实现方式，但其中往往存在很多类似的页面和组件，这些类似的组件会被抽离成一套标准规范。'
-  },
-  {
-    logo: 'https://gw.alipayobjects.com/zos/rmsportal/kZzEzemZyKLKFsojXItE.png',
-    name: 'React',
-    desc: '在中台产品的研发过程中，会出现不同的设计规范和实现方式，但其中往往存在很多类似的页面和组件，这些类似的组件会被抽离成一套标准规范。'
-  },
-  {
-    logo: 'https://gw.alipayobjects.com/zos/rmsportal/ComBAopevLwENQdKWiIn.png',
-    name: 'Vue',
-    desc: '在中台产品的研发过程中，会出现不同的设计规范和实现方式，但其中往往存在很多类似的页面和组件，这些类似的组件会被抽离成一套标准规范。'
-  },
-  {
-    logo: 'https://gw.alipayobjects.com/zos/rmsportal/nxkuOJlFJuAUhzlMTCEe.png',
-    name: 'Webpack',
-    desc: '在中台产品的研发过程中，会出现不同的设计规范和实现方式，但其中往往存在很多类似的页面和组件，这些类似的组件会被抽离成一套标准规范。'
-  }
-]
-
 export default [
-  // 树形列表接口
-  {
-    url: '/mock/example/treeList',
-    method: 'get',
-    timeout,
-    response: ({ query }) => {
-      const { title, pageIndex, pageSize } = query
-      const mockList = treeList.filter((item) => {
-        if (title && item.title.indexOf(title) < 0) return false
-        return true
-      })
-      const pageList = mockList.filter(
-        (_, index) => index < pageSize * pageIndex && index >= pageSize * (pageIndex - 1)
-      )
-      return {
-        code: SUCCESS_CODE,
-        data: {
-          total: mockList.length,
-          list: pageList
-        }
-      }
-    }
-  },
   // 列表接口
   {
-    url: '/mock/example/list',
+    url: '/markov_sim/api/v1/system_config/list',
     method: 'get',
     timeout,
     response: ({ query }) => {
-      const { title, pageIndex, pageSize } = query
-      const mockList = List.filter((item) => {
-        if (title && item.title.indexOf(title) < 0) return false
-        return true
-      })
-      const pageList = mockList.filter(
-        (_, index) => index < pageSize * pageIndex && index >= pageSize * (pageIndex - 1)
+      const { page = 1, limit = 10 } = query
+      const pageList = List.filter(
+        (_, index) => index < limit * page && index >= limit * (page - 1)
       )
       return {
         code: SUCCESS_CODE,
         data: {
-          total: mockList.length,
+          total: List.length,
           list: pageList
         }
       }
@@ -224,38 +118,39 @@ export default [
   },
   // 保存接口
   {
-    url: '/mock/example/save',
+    url: '/markov_sim/api/v1/system_config/create',
     method: 'post',
     timeout,
     response: ({ body }) => {
-      if (!body.id) {
-        List = [
-          Object.assign(body, {
-            id: toAnyString()
-          })
-        ].concat(List)
-        return {
-          code: SUCCESS_CODE,
-          data: 'success'
-        }
-      } else {
-        List.map((item) => {
-          if (item.id === body.id) {
-            for (const key in item) {
-              item[key] = body[key]
-            }
-          }
-        })
-        return {
-          code: SUCCESS_CODE,
-          data: 'success'
-        }
-      }
+      const {
+        name,
+        type,
+        created_at,
+        updated_at,
+        matrix,
+        vector,
+        men1,
+        men2,
+        processing_mode,
+        netWorks
+      } = body
+      List.push({
+        name,
+        type,
+        created_at,
+        updated_at,
+        matrix,
+        vector,
+        men1,
+        men2,
+        processing_mode,
+        netWorks
+      })
     }
   },
   // 详情接口
   {
-    url: '/mock/example/detail',
+    url: '/markov_sim/api/v1/system_config/detail',
     method: 'get',
     response: ({ query }) => {
       const { id } = query
@@ -271,7 +166,7 @@ export default [
   },
   // 删除接口
   {
-    url: '/mock/example/delete',
+    url: '/markov_sim/api/v1/system_config/delete',
     method: 'post',
     response: ({ body }) => {
       const ids = body.ids
@@ -281,37 +176,10 @@ export default [
           message: '请选择需要删除的数据'
         }
       } else {
-        let i = List.length
-        while (i--) {
-          if (ids.indexOf(List[i].id) !== -1) {
-            List.splice(i, 1)
-          }
-        }
+        List = List.filter((item) => !ids.includes(item.id))
         return {
           code: SUCCESS_CODE,
-          data: 'success'
-        }
-      }
-    }
-  },
-  {
-    url: '/mock/card/list',
-    method: 'get',
-    timeout,
-    response: ({ query }) => {
-      const { name, pageIndex, pageSize } = query
-      const mockList = cardList.filter((item) => {
-        if (name && item.name.indexOf(name) < 0) return false
-        return true
-      })
-      const pageList = mockList.filter(
-        (_, index) => index < pageSize * pageIndex && index >= pageSize * (pageIndex - 1)
-      )
-      return {
-        code: SUCCESS_CODE,
-        data: {
-          total: mockList.length,
-          list: pageList
+          message: '删除成功'
         }
       }
     }
