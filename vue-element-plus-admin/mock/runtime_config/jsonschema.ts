666e3fcf19879bd1b4ba76204716e80669c762c1
amber-yl <903582377@qq.com>
2025-06-10 09:30:58 +0800

    feat: 构建jsonschema

--- vue-element-plus-admin/mock/runtime_config/jsonschema.ts
+++ vue-element-plus-admin/mock/runtime_config/jsonschema.ts
@@ -40,17 +40,24 @@ export const enhancedInferenceModelConfigSchema = {
         sequence_length_list: {
           description: '负载输入输出长度，可设置多个',
           title: 'Sequence Length List',
-          anyOf: [
-            {
-              type: 'array',
-              items: {
-                type: 'integer'
+          type: 'array',
+          items: {
+            type: 'object',
+            required: ['input_seq_length', 'output_seq_length'],
+            title: 'Sequence Length',
+            properties: {
+              input_seq_length: {
+                description: '输入长度',
+                type: 'integer',
+                title: 'Input Seq Length'
+              },
+              output_seq_length: {
+                description: '输出长度',
+                type: 'integer',
+                title: 'Output Seq Length'
               }
-            },
-            {
-              type: 'null'
             }
-          ]
+          }
         },
         num_procs_list: {
           description: '总卡数，PD分离时为P卡+D卡',
@@ -63,65 +70,361 @@ export const enhancedInferenceModelConfigSchema = {
         wac_bytes_list: {
           description: '依次为[weight_quant, activation_quant, cache_quant, communication_quant]',
           title: 'Wac Bytes List',
-          type: 'array'
+          type: 'array',
+          items: {
+            type: 'object',
+            required: ['weight_quant', 'activation_quant', 'cache_quant', 'communication_quant'],
+            title: 'Wac Bytes',
+            properties: {
+              weight_quant: {
+                description: '权重精度',
+                type: 'integer',
+                title: 'Weight Quant'
+              },
+              activation_quant: {
+                description: '激活精度',
+                type: 'integer',
+                title: 'Activation Quant'
+              },
+              cache_quant: {
+                description: '缓存精度',
+                type: 'integer',
+                title: 'Cache Quant'
+              },
+              communication_quant: {
+                description: '通信精度',
+                type: 'integer',
+                title: 'Communication Quant'
+              }
+            }
+          }
         },
         flops_type_list: {
           description: '依次为[matrix_flops_type, attn_flops_type, vector_flops_type]',
           anyOf: [],
           title: 'Flops Type List',
-          type: 'array'
+          type: 'array',
+          items: {
+            type: 'object',
+            required: ['matrix_flops_type', 'attn_flops_type', 'vector_flops_type'],
+            title: 'Flops Type',
+            properties: {
+              matrix_flops_type: {
+                description: '矩阵运算精度',
+                type: 'integer',
+                title: 'Matrix Flops Type'
+              },
+              attn_flops_type: {
+                description: '注意力运算精度',
+                type: 'integer',
+                title: 'Attn Flops Type'
+              },
+              vector_flops_type: {
+                description: '向量运算精度',
+                type: 'integer',
+                title: 'Vector Flops Type'
+              }
+            }
+          }
         },
         time_limit_list: {
           description: 'TTFT和TPOT约束，单位ms，无约束时可配置[null, null]',
           title: 'Time Limit List',
-          type: 'array'
+          type: 'array',
+          items: {
+            type: 'object',
+            required: ['TTFT', 'TPOT'],
+            title: 'Time Limit',
+            properties: {
+              TTFT: {
+                description: 'TTFT约束',
+                title: 'Ttft',
+                anyOf: [
+                  {
+                    type: 'integer'
+                  },
+                  {
+                    type: 'null'
+                  }
+                ]
+              },
+              TPOT: {
+                description: 'TPOT约束',
+                title: 'Tpot',
+                anyOf: [
+                  {
+                    type: 'integer'
+                  },
+                  {
+                    type: 'null'
+                  }
+                ]
+              }
+            }
+          }
         },
         sys_list: {
           description: '芯片配置，PD分离时需分别配置P芯片和D芯片，PD融合时仅配置一个芯片',
           title: 'Sys List',
-          type: 'array'
+          type: 'array',
+          items: {
+            type: 'string'
+          }
         },
         pd_num_procs_list: {
           description: '当PD分离的时候，可指定P/D各自的数目；缺省None，自动寻优最佳PD配置',
           title: 'Pd Num Procs List',
-          type: 'array'
+          default: null,
+          anyOf: [
+            {
+              type: 'array',
+              items: {
+                type: 'object',
+                required: ['p_num_procs', 'd_num_procs'],
+                title: 'pd_num_procs_list',
+                properties: {
+                  p_num_procs: {
+                    description: 'P卡数目',
+                    type: 'integer',
+                    title: 'P Num Procs'
+                  },
+                  d_num_procs: {
+                    description: 'D卡数目',
+                    type: 'integer',
+                    title: 'D Num Procs'
+                  }
+                }
+              }
+            },
+            {
+              type: null
+            }
+          ]
         },
         p_parallel_config: {
           description:
             '指定P阶段的运行时策略[DP,PP,TP]，当前pp未实现，默认1；MoE模型为Attn部分运行时策略',
           title: 'P Parallel Config',
-          type: 'object'
+          default: null,
+          anyOf: [
+            {
+              type: 'array',
+              items: {
+                type: 'object',
+                title: 'P Parallel Config',
+                required: ['DP', 'PP', 'TP'],
+                properties: {
+                  DP: {
+                    description: 'P阶段DP策略',
+                    type: 'integer',
+                    title: 'Dp'
+                  },
+                  PP: {
+                    description: 'P阶段PP策略',
+                    type: 'integer',
+                    title: 'Pp'
+                  },
+                  TP: {
+                    description: 'P阶段TP策略',
+                    type: 'integer',
+                    title: 'Tp'
+                  }
+                }
+              }
+            },
+            {
+              type: null
+            }
+          ]
         },
         d_parallel_config: {
           description:
             '指定D阶段的运行时策略[DP,PP,TP]，当前pp未实现，默认1；MoE模型为Attn部分运行时策略',
           title: 'D Parallel Config',
-          type: 'object'
+          default: null,
+          anyOf: [
+            {
+              type: 'array',
+              items: {
+                type: 'object',
+                title: 'D Parallel Config',
+                required: ['DP', 'PP', 'TP'],
+                properties: {
+                  DP: {
+                    description: 'D阶段DP策略',
+                    type: 'integer',
+                    title: 'Dp'
+                  },
+                  PP: {
+                    description: 'D阶段PP策略',
+                    type: 'integer',
+                    title: 'Pp'
+                  },
+                  TP: {
+                    description: 'D阶段TP策略',
+                    type: 'integer',
+                    title: 'Tp'
+                  }
+                }
+              }
+            }
+          ]
         },
         p_moe_parallel_config: {
           description: '仅MoE模型生效，指定P阶段的MoE运行时策略[EP, Moe_TP, Moe_DP]',
           title: 'P MoE Parallel Config',
-          type: 'object'
+          default: null,
+          anyOf: [
+            {
+              type: 'array',
+              items: {
+                type: 'object',
+                title: 'P MoE Parallel Config',
+                required: ['EP', 'Moe_TP', 'Moe_DP'],
+                properties: {
+                  EP: {
+                    description: 'P阶段EP策略',
+                    type: 'integer',
+                    title: 'Ep'
+                  },
+                  Moe_TP: {
+                    description: 'P阶段Moe_TP策略',
+                    type: 'integer',
+                    title: 'Moe Tp'
+                  },
+                  Moe_DP: {
+                    description: 'P阶段Moe_DP策略',
+                    type: 'integer',
+                    title: 'Moe Dp'
+                  }
+                }
+              }
+            },
+            {
+              type: null
+            }
+          ]
         },
         d_moe_parallel_config: {
           description: '仅MoE模型生效，指定D阶段的MoE运行时策略[EP, Moe_TP, Moe_DP]',
           title: 'D MoE Parallel Config',
-          type: 'object'
+          default: null,
+          anyOf: [
+            {
+              type: 'array',
+              items: {
+                type: 'object',
+                title: 'D MoE Parallel Config',
+                required: ['EP', 'Moe_TP', 'Moe_DP'],
+                properties: {
+                  EP: {
+                    description: 'D阶段EP策略',
+                    type: 'integer',
+                    title: 'Ep'
+                  },
+                  Moe_TP: {
+                    description: 'D阶段Moe_TP策略',
+                    type: 'integer',
+                    title: 'Moe Tp'
+                  },
+                  Moe_DP: {
+                    description: 'D阶段Moe_DP策略',
+                    type: 'integer',
+                    title: 'Moe Dp'
+                  }
+                }
+              }
+            }
+          ]
         },
         p_micro_batch_size: {
           description: 'P阶段batch_size设置[global_bs, num_bs, bs],建议默认[1,1,1]节省寻优时间',
           title: 'P Micro Batch Size',
-          type: 'integer'
+          default: null,
+          anyOf: [
+            {
+              type: 'array',
+              items: {
+                type: 'object',
+                title: 'P Micro Batch Size',
+                required: ['global_bs', 'num_bs', 'bs'],
+                properties: {
+                  global_bs: {
+                    description: '全局batch_size',
+                    type: 'integer',
+                    title: 'Global Bs'
+                  },
+                  num_bs: {
+                    description: 'num_bs',
+                    type: 'integer',
+                    title: 'Num Bs'
+                  },
+                  bs: {
+                    description: 'bs',
+                    type: 'integer',
+                    title: 'Bs'
+                  }
+                }
+              }
+            },
+            {
+              type: null
+            }
+          ]
         },
         d_micro_batch_size: {
           description: 'D阶段batch_size设置[global_bs, num_bs, bs],建议默认[32,1,32]',
           title: 'D Micro Batch Size',
-          type: 'integer'
+          default: null,
+          anyOf: [
+            {
+              type: 'array',
+              items: {
+                type: 'object',
+                title: 'D Micro Batch Size',
+                required: ['global_bs', 'num_bs', 'bs'],
+                properties: {
+                  global_bs: {
+                    description: '全局batch_size',
+                    type: 'integer',
+                    title: 'Global Bs'
+                  },
+                  num_bs: {
+                    description: 'num_bs',
+                    type: 'integer',
+                    title: 'Num Bs'
+                  },
+                  bs: {
+                    description: 'bs',
+                    type: 'integer',
+                    title: 'Bs'
+                  }
+                }
+              }
+            },
+            {
+              type: null
+            }
+          ]
         },
         num_redundant_expert_config: {
           description: '仅MoE模型decode生效，D阶段基于卡数寻优不同冗余专家个数时的运行时策略',
           title: 'Num Redundant Expert Config',
-          type: 'integer'
+          default: null,
+          anyOf: [
+            {
+              type: 'array',
+              items: {
+                type: 'integer'
+              }
+            },
+            {
+              type: 'integer'
+            },
+            {
+              type: null
+            }
+          ]
         },
         deploy_shared_expert_config: {
           anyOf: [
